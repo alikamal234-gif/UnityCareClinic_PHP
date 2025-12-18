2aee3ca006096fd6cd6192fef7d3f5c7342c22b3
alikamal234-gif <alikamal0675@gmail.com>
2025-12-18 15:30:35 +0100

    HEAL-26: Les requétes de SQL Statistiques dans le file de sql

--- sql/data.sql
+++ sql/data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 17, 2025 at 11:20 AM
+-- Generation Time: Dec 18, 2025 at 02:28 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -40,7 +40,12 @@ CREATE TABLE `departments` (
 INSERT INTO `departments` (`id_department`, `department_name`, `location`) VALUES
 (15, 'Inga Small', 'Atque excepturi vita'),
 (16, 'Althea Jefferson', 'Dicta neque ullam mo'),
-(25, 'Desiree Moody', 'Qui exercitationem u');
+(25, 'Desiree Moody', 'Qui exercitationem u'),
+(26, 'Simon Cooper', 'Est quo aut digniss'),
+(27, 'Simon Cooper', 'Unde et consequatur'),
+(28, 'Garth Lloyd', 'Occaecat non minim i'),
+(29, 'Cameron Whitney', 'Omnis eos odit esse '),
+(30, 'Karina Richardson', 'Sit sed mollitia pr');
 
 -- --------------------------------------------------------
 
@@ -67,7 +72,12 @@ INSERT INTO `doctors` (`id_doctor`, `first_name`, `last_name`, `specialization`,
 (2, 'Olga', 'Chan', 'Quis reprehenderit t', '+1 (629) 212-8128', 'gyrocima@mailinator.com', 15),
 (3, 'Orla', 'Rowe', 'Quos ea itaque eum o', '+1 (371) 747-4217', 'roxelaruke@mailinator.com', 16),
 (4, 'Pascale', 'Cummings', 'Hic laborum Nisi la', '+1 (513) 856-5992', 'vajyxyhu@mailinator.com', 15),
-(8, 'Declan', 'Wooten', 'Nihil non tempor nul', '+1 (956) 108-7448', 'niramob@mailinator.com', 25);
+(8, 'Declan', 'Wooten', 'Nihil non tempor nul', '+1 (956) 108-7448', 'niramob@mailinator.com', 25),
+(9, 'Desirae', 'Chan', 'Voluptate pariatur ', '+1 (657) 554-2362', 'mahyred@mailinator.com', 15),
+(13, 'Ronan', 'Travis', 'Fugiat animi eligen', '+1 (609) 186-7004', 'ruwomiz@mailinator.com', 16),
+(16, 'Colby', 'Vang', 'Aute consequat Assu', '1', 'bomafocu@mailinator.com', 27),
+(17, 'Alice', 'Neal', 'Ea eos neque cum ip', '1', 'nowunan@mailinator.com', 27),
+(18, 'Sloane', 'Dunlap', 'Est enim officia tem', '+1 (249) 898-2584', 'byraxek@mailinator.com', 15);
 
 -- --------------------------------------------------------
 
@@ -98,7 +108,10 @@ INSERT INTO `patients` (`id_patient`, `first_name`, `last_name`, `gender`, `emai
 (6, 'Ian', 'Butler', 'female', 'kusykaqy@mailinator.com', '1995-05-08', '+1 (372) 969-3313', 'Omnis nisi quod tota'),
 (7, 'Colt', 'Holcomb', 'male', 'teki@mailinator.com', '1981-08-03', '+1 (905) 388-8647', 'Reiciendis maxime fu'),
 (8, 'Sarah', 'Hendricks', 'male', 'hyji@mailinator.com', '2017-11-23', '+1 (697) 282-8326', 'Nostrud ipsam odio c'),
-(9, 'Juliet', 'Mosley', 'male', 'didyjugyz@mailinator.com', '1993-11-26', '+1 (892) 325-6843', 'Autem eiusmod minima');
+(9, 'Juliet', 'Mosley', 'male', 'didyjugyz@mailinator.com', '1993-11-26', '+1 (892) 325-6843', 'Autem eiusmod minima'),
+(10, 'Caleb', 'Melendez', 'male', 'sibohe@mailinator.com', '1980-06-16', '0', 'Adipisicing in beata'),
+(11, 'Imani', 'Griffith', 'male', '', '1971-01-14', '0', 'Omnis sit modi anim'),
+(12, 'Alika', 'Aguirre', 'female', 'hyloqojaj@mailinator.com', '1974-01-13', '1', 'Sed eu eveniet nece');
 
 --
 -- Indexes for dumped tables
@@ -131,19 +144,19 @@ ALTER TABLE `patients`
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id_department` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_department` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id_doctor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_doctor` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id_patient` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_patient` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -155,7 +168,3 @@ ALTER TABLE `patients`
 ALTER TABLE `doctors`
   ADD CONSTRAINT `fk_doctors_department` FOREIGN KEY (`id_department`) REFERENCES `departments` (`id_department`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
